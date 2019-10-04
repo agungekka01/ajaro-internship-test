f58af181d1912f3d17fb7e98efcf0ba4769ab4dc
agungekka01 <agungekka01@gmail.com>
2019-10-04 16:12:52 +0700

    aku

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+ 
+$host   = 'localhost';
+$user   = 'root';
+$pass   = '';
+$db     = 'barang';
+ 
+$conn   = mysqli_connect($host, $user, $pass, $db) or die ("Koneksi ke database GAGAL!!!");
+ 
+
+    ?>
+
